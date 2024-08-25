b34fb4b5fd0dbe240e0001877b92eeee3c317987
JoseG <josegerman@users.noreply.github.com>
2024-08-25 19:06:46 -0400

    update

--- 2_prompt_templates/template_basics.py
+++ 2_prompt_templates/template_basics.py
@@ -1,18 +1,24 @@
+# ===========================
 # Prompt Template Docs:
-#   https://python.langchain.com/v0.2/docs/concepts/#prompt-templateshttps://python.langchain.com/v0.2/docs/concepts/#prompt-templates
+# ===========================
+# https://python.langchain.com/v0.2/docs/concepts/#prompt-templateshttps://python.langchain.com/v0.2/docs/concepts/#prompt-templates
 
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage
 
-# # PART 1: Create a ChatPromptTemplate using a template string
-# template = "Tell me a joke about {topic}."
-# prompt_template = ChatPromptTemplate.from_template(template)
+# ===========================
+# PART 1: Create a ChatPromptTemplate using a template string
+# ===========================
+template = "Tell me a joke about {topic}."
+prompt_template = ChatPromptTemplate.from_template(template)
 
-# print("-----Prompt from Template-----")
-# prompt = prompt_template.invoke({"topic": "cats"})
-# print(prompt)
+print("-----Prompt from Template-----")
+prompt = prompt_template.invoke({"topic": "cats"})
+print(prompt) 
 
+# ===========================
 # # PART 2: Prompt with Multiple Placeholders
+# ===========================
 # template_multiple = """You are a helpful assistant.
 # Human: Tell me a {adjective} story about a {animal}.
 # Assistant:"""
@@ -21,8 +27,9 @@ from langchain_core.messages import HumanMessage
 # print("\n----- Prompt with Multiple Placeholders -----\n")
 # print(prompt)
 
-
+# ===========================
 # PART 3: Prompt with System and Human Messages (Using Tuples)
+# ===========================
 # messages = [
 #     ("system", "You are a comedian who tells jokes about {topic}."),
 #     ("human", "Tell me {joke_count} jokes."),
@@ -32,7 +39,9 @@ from langchain_core.messages import HumanMessage
 # print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
 # print(prompt)
 
-# # Extra Informoation about Part 3.
+# ===========================
+# # Extra Information about Part 3.
+# ===========================
 # # This does work:
 # messages = [
 #     ("system", "You are a comedian who tells jokes about {topic}."),
@@ -43,13 +52,15 @@ from langchain_core.messages import HumanMessage
 # print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
 # print(prompt)
 
-
+# ===========================
 # This does NOT work:
-messages = [
-    ("system", "You are a comedian who tells jokes about {topic}."),
-    HumanMessage(content="Tell me {joke_count} jokes."),
-]
-prompt_template = ChatPromptTemplate.from_messages(messages)
-prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
-print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
-print(prompt)
+# ===========================
+# You cannot do interpolation on the HumanMessage or inserting a value in {joke_count}
+# messages = [
+#     ("system", "You are a comedian who tells jokes about {topic}."),
+#     HumanMessage(content="Tell me {joke_count} jokes."),
+# ]
+# prompt_template = ChatPromptTemplate.from_messages(messages)
+# prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
+# print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
+# print(prompt)
